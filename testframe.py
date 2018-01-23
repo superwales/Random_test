00a7ecddb206d85cd995ae07311706dd45314288
superwales <34479855+superwales@users.noreply.github.com>
2018-01-23 19:59:51 +0800

    引用更新
    现在测试模式可以得到带括号的题

--- testframe.py
+++ testframe.py
@@ -1,4 +1,7 @@
 # -*- coding:UTF-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import wx
 import v
 import time
@@ -14,7 +17,7 @@ class PreFrame(wx.Frame):
         #在面板上添加控件
         self.quesnum= wx.StaticText(panel, label="题目个数:", pos=(20, 20))
         self.quesran = wx.StaticText(panel, label="运算范围:", pos=(20, 70))
-        self.questime = wx.StaticText(panel, label="考试时间:", pos=(20, 120))
+        self.questime = wx.StaticText(panel, label="考试时间(min):", pos=(7, 120))
         self.num = wx.TextCtrl(panel,size=(70,25), pos=(90, 20))
         self.ran = wx.TextCtrl(panel, size=(70,25), pos=(90, 70))
         self.time = wx.TextCtrl(panel, size=(70,25), pos=(90, 120))
@@ -33,48 +36,139 @@ class PreFrame(wx.Frame):
         elif self.time.GetValue()=="":
             wx.MessageBox("请输入考试时间！")
         else:
-            v.ques_num=self.num.GetValue()
-            v.ques_ran=self.ran.GetValue()
-            v.ques_time=self.time.GetValue()
+            v.ques_num=int(self.num.GetValue())
+            v.ques_ran=int(self.ran.GetValue())
+            v.ques_time=int(self.time.GetValue())
+            v.ques_time=v.ques_time*60
+            (q,a,s,m)=getquestionlist(v.ques_num,v.ques_ran)
+            v.questionlist=q
+            v.anslist=a
+            v.scorelist=s
+            v.myanslist=m
             self.Close()
             testframe=TestFrame(None,-1)
 
 class TestFrame(wx.Frame):
 
     def __init__(self,parent,id):
-        wx.Frame.__init__(self,parent,-1,'随机练习',size=(500,350))
+        wx.Frame.__init__(self,parent,-1,'测试模式',size=(350,200))
         #创建，面板
         panel=wx.Panel(self)
 
         #在面板上添加控件
-        self.quesnum= wx.StaticText(panel, label="运算范围:", pos=(5, 20))
-        self.quesname = wx.StaticText(panel, label="题目:", pos=(20, 50))
-        self.an = wx.StaticText(panel, label="作答:", pos=(20, 170))
-        self.time = wx.StaticText(panel, label="用时:", pos=(330, 20))
-        self.totalnum = wx.StaticText(panel, label="已答题数:"+str(v.total_num), pos=(330, 100))
-        self.correctnum = wx.StaticText(panel, label="正确题数:"+str(v.correct_num), pos=(330, 60))
-        self.c = wx.StaticText(panel, label="交互栏", pos=(330, 140))
-        self.qran = wx.TextCtrl(panel, pos=(60, 20))
-        self.question = wx.TextCtrl(panel, size=(250,100),style=wx.TE_MULTILINE, pos=(60, 50))
-        self.answ=wx.TextCtrl(panel,size=(250,70),pos=(60,170))
-        self.con = wx.TextCtrl(panel, size=(150, 120), style=wx.TE_MULTILINE, pos=(330, 170))
+        self.questh= wx.StaticText(panel, label="第1题", pos=(5, 20))
+        self.ques = wx.StaticText(panel, label=v.questionlist[0], pos=(40, 20))
+        self.qnum = wx.StaticText(panel,  pos=(200, 20))
+        self.time = wx.StaticText(panel,  pos=(200, 70))
+        self.ans = wx.StaticText(panel, label="作答:", pos=(5, 70))
+        self.an = wx.TextCtrl(panel, pos=(40, 70))
+
         self.timer = wx.Timer(self)  # 创建定时器
-        sta = wx.Button(panel, label='开始答题',size=(70,30) ,pos=(15, 270))
-        firm=wx.Button(panel,label='确认',size=(70,30) ,pos=(95,270))
-        nex = wx.Button(panel, label='下一题', size=(70,30) ,pos=(175, 270))
-        en = wx.Button(panel, label='结束答题', size=(70,30) ,pos=(255, 270))
+        las = wx.Button(panel, label='上一题',size=(70,30) ,pos=(10, 120))
+        firm=wx.Button(panel,label='提交',size=(70,30) ,pos=(250,120))
+        nex = wx.Button(panel, label='下一题', size=(70,30) ,pos=(170, 120))
+        tem = wx.Button(panel, label='缓存', size=(70, 30), pos=(90, 120))
 
-        #self.Bind(wx.EVT_BUTTON, self.OnSta, sta)
-        #self.Bind(wx.EVT_BUTTON, self.OnFirm, firm)
-        #self.Bind(wx.EVT_BUTTON, self.OnNex, nex)
-        #self.Bind(wx.EVT_BUTTON,self.OnEn,en)
-        #self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)  # 绑定一个定时器事件
-        #self.timer.Start(1000)  # 设定时间间隔
+        self.Bind(wx.EVT_BUTTON, self.OnLas, las)
+        self.Bind(wx.EVT_BUTTON, self.OnFirm, firm)
+        self.Bind(wx.EVT_BUTTON, self.OnNex, nex)
+        self.Bind(wx.EVT_BUTTON, self.OnTem,tem)
 
+        self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)  # 绑定一个定时器事件
+        self.timer.Start(1000)  # 设定时间间隔
+        (h, m, s) = transtime(v.ques_time)
+        self.time.SetLabel("剩余时间：" + str(h) + ":" + str(m) + ":" + str(s))
+        self.qnum.SetLabel("已答题数：" + str(0) + "/" + str(v.ques_num))
         self.Show(True)
 
+    def OnLas(self,e):
+        if v.quesorder==0:
+            wx.MessageBox("已经是第一题！")
+        else:
+            v.quesorder=v.quesorder-1
+            order=v.quesorder+1
+            self.questh.SetLabel("第%d题"%order)
+            self.ques.SetLabel(str(v.questionlist[v.quesorder]))
+            self.an.SetValue(v.myanslist[v.quesorder])
+    def OnFirm(self,e):
+        for i in range(0,v.ques_num):
+            if str(v.myanslist[i]) == str(v.anslist[i]):
+                v.grade=v.grade+v.scorelist[i]
+        gra=GradeFrame(None,-1)
+        self.Close()
+    def OnNex(self,e):
+        if v.quesorder==v.ques_num-1:
+            wx.MessageBox("已经是最后一题！")
+        else:
+            v.quesorder = v.quesorder + 1
+            order = v.quesorder + 1
+            self.questh.SetLabel("第%d题"%order)
+            self.ques.SetLabel(str(v.questionlist[v.quesorder]))
+            self.an.SetValue(v.myanslist[v.quesorder])
+    def OnTem(self,e):
+        v.myanslist[v.quesorder] = self.an.GetValue()
+        t=0
+        for ietms in v.myanslist:
+            if ietms!="":
+                t=t+1
+        self.qnum.SetLabel("已答题数："+str(t)+"/"+str(v.ques_num))
+    def OnTimer(self,e):
+        v.ques_time=v.ques_time-1
+        (h,m,s)=transtime(v.ques_time)
+        self.time.SetLabel("剩余时间："+str(h)+":"+str(m)+":"+str(s))
+        if v.ques_time==300:
+            wx.MessageBox("答题时间还有5分钟！")
+        elif v.ques_time==0:
+            self.Close()
+            grade = GradeFrame(None, -1)
 
+class GradeFrame(wx.Frame):
+    def __init__(self,parent,id):
+        wx.Frame.__init__(self,parent,-1,'测试结果',size=(300,450))
+        #创建，面板
+        panel=wx.Panel(self)
+
+        #在面板上添加控件
+        self.n= wx.StaticText(panel, label="成绩单", pos=(120, 20))
+        self.detail = wx.TextCtrl(panel, size=(270, 300),style=wx.TE_MULTILINE, pos=(10, 40))
+        save = wx.Button(panel, label='保存', size=(70, 30), pos=(50, 370))
+        exi = wx.Button(panel, label='退出', size=(70, 30), pos=(150, 370))
+
+        self.detail.SetValue(str(v.username)+"    您本次的成绩为："+str(v.grade)+"分\n")
+
+        for i in range(0,v.ques_num):
+            self.detail.AppendText("第%d题："%(i+1)+str(v.questionlist[i])+str(v.scorelist[i])+"\n")
+            if str(v.myanslist[i])==str(v.anslist[i]):
+                self.detail.AppendText("回答正确！  正确答案是："+str(v.anslist[i])+"("+str(v.myanslist[i])+")\n")
+            else:
+                self.detail.AppendText("回答错误！  正确答案是："+str(v.anslist[i])+"("+str(v.myanslist[i])+")\n")
+
+        self.Bind(wx.EVT_BUTTON, self.OnSave, save)
+        self.Bind(wx.EVT_BUTTON, self.OnExi, exi)
+
+
+        self.Show()
+    def OnSave(self,e):
+        filename=v.username+"_test.txt"
+        filename1 = v.username + "_error.txt"
+        filename2 = v.username + "_history.txt"
+        with open(filename,'w') as file_object:
+            file_object.write(str(v.username)+"    您本次的成绩为："+str(v.grade)+"分\n")
+            for i in range(0, v.ques_num):
+                with open(filename2,'a') as fileobject:
+                    fileobject.write(str(v.questionlist[i]) + "||" + str(v.anslist[i]) + "||" + str(v.myanslist[i]) + "\n")
+                file_object.write("第%d题：" % (i + 1) + str(v.questionlist[i]) + "\n")
+                if str(v.myanslist[i]) == str(v.anslist[i]):
+                    file_object.write("回答正确！  正确答案是："+str(v.anslist[i])+"("+str(v.myanslist[i])+")\n")
+                else:
+                    file_object.write("回答错误！  正确答案是：" + str(v.anslist[i]) + "(" + str(v.myanslist[i]) + ")\n")
+                    with open(filename1,'a') as fileobject:
+                        fileobject.write(str(v.questionlist[i]) + "||" + str(v.anslist[i]) + "||" + str(v.myanslist[i]) + "\n")
+
+    def OnExi(self,e):
+        self.Close()
+'''
 app=wx.App(False)
-frame=TestFrame(None,-1)
+frame=PreFrame(None,-1)
 
-app.MainLoop()
+app.MainLoop()'''
