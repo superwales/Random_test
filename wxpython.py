ee6a93ee2562cec74ddfeaec19812c00d2567430
superwales <34479855+superwales@users.noreply.github.com>
2018-01-23 19:56:25 +0800

    界面更新，添加题目工厂

--- wxpython.py
+++ wxpython.py
@@ -1,41 +1,60 @@
 # -*- coding:UTF-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from Wr_quesframe import *
+from testframe import *
+from historyframe import *
+from collectframe import *
+from replace import *
+from diyques import *
 import wx
-
+#创建主界面
 class MainFrame(wx.Frame):
     def __init__(self,parent,id):
-        wx.Frame.__init__(self,parent,-1,'炜柒运算',size=(200,350))
+        wx.Frame.__init__(self,parent,-1,'四则运算',size=(200,350))
         #创建，面板
         panel=wx.Panel(self)
         #在面板上添加控件
-        self.usinamelabel= wx.StaticText(panel, label="炜柒:", pos=(30, 30))
-        self.correct_rate= wx.StaticText(panel, label="成功率", pos=(100, 30))
-        prac=wx.Button(panel,label="练习模式",pos=(50,70))
-        exam=wx.Button(panel,label="测试模式",pos=(50,120))
-        rew = wx.Button(panel, label="错题本", pos=(50, 170))
-        reh = wx.Button(panel, label="历史记录", pos=(50, 220))
+        self.usinamelabel= wx.StaticText(panel, label=" ", pos=(60, 10))
+        self.usinamelabel.SetLabel(v.username)
+        #self.correct_rate= wx.StaticText(panel, label="成功率", pos=(100, 30))
+        randprac = wx.Button(panel, label="随机练习", pos=(50, 30))
+        prac = wx.Button(panel, label="针对练习", pos=(50, 70))
+        exam = wx.Button(panel, label="测试模式", pos=(50, 110))
+        diyques = wx.Button(panel, label="题目工厂", pos=(50, 150))
+        rew = wx.Button(panel, label="收藏夹", pos=(50, 190))
+        reh = wx.Button(panel, label="历史记录", pos=(50, 230))
         exi = wx.Button(panel, label="退出", pos=(50, 270))
 
-
+        self.Bind(wx.EVT_BUTTON, self.Onrandprac, randprac)
         self.Bind(wx.EVT_BUTTON, self.Onprac, prac)
         self.Bind(wx.EVT_BUTTON, self.Onexam, exam)
+        self.Bind(wx.EVT_BUTTON, self.Ondiyques, diyques)
         self.Bind(wx.EVT_BUTTON, self.Onrew, rew)
         self.Bind(wx.EVT_BUTTON, self.Onreh, reh)
         self.Bind(wx.EVT_BUTTON, self.Onexi, exi)
 
         self.Show(True)
 
+    def Onrandprac(self,e):
+        #self.Close(True)
+        rand=PracFrame(None,-1)
     def Onprac(self,e):
-       i=1
+        replace=ReplaceFrame(None,-1)
     def Onexam(self,e):
-        i=1
+        exam=PreFrame(None,-1)
+    def Ondiyques(self,e):
+        diy=DiyFrame(None,-1)
     def Onrew(self,e):
-        i=1
+        collect=CollectFrame(None,-1)
     def Onreh(self,e):
-        i=1
+        his=HistoryFrame(None,-1)
     def Onexi(self,e):
         self.Close(True)
 
-class MyFrame(wx.Frame):
+#创建登录界面
+class LoFrame(wx.Frame):
     def __init__(self,parent,id):
         wx.Frame.__init__(self,parent,-1,'炜柒运算',size=(280,200))
         #创建，面板
@@ -72,6 +91,13 @@ class MyFrame(wx.Frame):
                         result=True
                 if result==True:
                     wx.MessageBox("登录成功！")
+                    v.username=text_usr.GetText()
+                    filename1=v.username+"_error.txt"
+                    filename2=v.username+"_history.txt"
+                    f=open(filename1,'a')
+                    f.close()
+                    f=open(filename2,'a')
+                    f.close()
                     self.Close(True)
                     #打开主界面
                     mainframe=MainFrame(None,-1)
@@ -103,7 +129,10 @@ class MyFrame(wx.Frame):
                 else:
                     wx.MessageBox("用户名已被使用！")
 
-app=wx.App(False)
-frame=MyFrame(None,-1)
+def main():
+    app=wx.App(False)
+    frame=LoFrame(None,-1)
+    app.MainLoop()
 
-app.MainLoop()
+if __name__=='__main__':
+    main()
